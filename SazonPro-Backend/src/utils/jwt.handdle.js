433633cdeae8e374f89148151bc339d7a6d0f19b
Fabio-Alfredo <00210121@uca.edu.sv>
2024-09-06 22:11:53 -0600

    add: generation and verifyed token function

---
+++ SazonPro-Backend/src/utils/jwt.handdle.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
+
+const _JWT_SECRET = process.env.JWT_SECRET;
+
+export const generateToken = (data)=>{
+    const token = jwt.sign(
+        data,
+        _JWT_SECRET,
+        {expiresIn: '1h'}
+    )
+
+    return token;
+}
+
+export const verifyToken = (token)=>{
+    const decoded = jwt.verify(token, _JWT_SECRET)
+    return decoded;
+}
