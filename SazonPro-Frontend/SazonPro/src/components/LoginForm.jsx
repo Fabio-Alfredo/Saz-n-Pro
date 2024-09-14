20cac08bf0504ffba6141099cbd48e544bf4476e
Fabio-Alfredo <00210121@uca.edu.sv>
2024-09-13 23:41:15 -0600

    update: style responsive form

--- SazonPro-Frontend/SazonPro/src/components/LoginForm.jsx
+++ SazonPro-Frontend/SazonPro/src/components/LoginForm.jsx
@@ -2,24 +2,31 @@ import React from "react";
 
 const LoginForm = () => {
   return (
-    <>
-      <form className='grid grid-rows-3 gap-4 p-5'>
-        <div className='flex flex-col'>
-          <label>Your name:</label>
-          <input className='rounded-lg h-8' placeholder='Enter your name' />
-        </div>
+    <form className="grid grid-rows-3 gap-4 p-5">
+      <div className="flex flex-col">
+        <label>Your name:</label>
+        <input
+          className="rounded-lg h-8 w-11/12 pl-2"
+          placeholder="Enter your name"
+        />
+      </div>
 
-        <div className='flex flex-col'>
-          <label>Your email:</label>
-          <input className='rounded-lg h-8' placeholder='Enter your email' />
-        </div>
+      <div className="flex flex-col">
+        <label>Your email:</label>
+        <input
+          className="rounded-lg h-8 w-11/12 pl-2"
+          placeholder="Enter your email"
+        />
+      </div>
 
-        <div className='flex flex-col'>
-          <label>Your password:</label>
-          <input className='rounded-lg h-8' placeholder='Enter your password' />
-        </div>
-      </form>
-    </>
+      <div className="flex flex-col">
+        <label>Your password:</label>
+        <input
+          className="rounded-lg h-8 w-11/12 pl-2"
+          placeholder="Enter your password"
+        />
+      </div>
+    </form>
   );
 };
 
--- SazonPro-Frontend/SazonPro/src/pages/Login.jsx
+++ SazonPro-Frontend/SazonPro/src/pages/Login.jsx
@@ -4,7 +4,7 @@ import LoginForm from "../components/LoginForm";
 const Login = () => {
   return (
     <div className="h-screen w-full flex justify-center items-center bg-gradient-to-br from-primary via-secondary to-tertiary ">
-      <div className="h-fit w-70% grid grid-cols-2 bg-gray_dark">
+      <div className="h-fit w-70% mx-2 sm:mx-0 grid grid-cols-2 bg-gray_dark">
         <div className="">
           <h2 className="text-3xl text-center text-gray-50">Login</h2>
         </div>
