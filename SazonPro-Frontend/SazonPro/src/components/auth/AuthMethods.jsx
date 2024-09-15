444b51baeb2946c6001e1137468e7cfac2770efb
Fabio-Alfredo <00210121@uca.edu.sv>
2024-09-14 18:00:54 -0600

    udpate: responsive desing

--- SazonPro-Frontend/SazonPro/src/components/auth/AuthMethods.jsx
+++ SazonPro-Frontend/SazonPro/src/components/auth/AuthMethods.jsx
@@ -14,7 +14,7 @@ const AuthMethods = () => {
       </div>
       <div className="row-span-1 flex flex-col items-center py-5 gap-1">
         <p className="text-center font-sans">-- OR --</p>
-        <div className="flex justify-between w-full px-8">
+        <div className="flex  justify-between sm:justify-center sm:gap-10 w-full px-8">
           <button className="flex pl-2 items-center border-solid border border-gray h-10 w-36 rounded-xl cursor-pointer font-sans text-xs">
             <FaGithub className="text-2xl" />
             Sing up with Github
