483fcdbd22cfc35c99327343636bf484e5de2a42
Fabio-Alfredo <00210121@uca.edu.sv>
2024-09-18 21:50:41 -0600

    update: new responsive desing

--- SazonPro-Frontend/SazonPro/src/pages/Login.jsx
+++ SazonPro-Frontend/SazonPro/src/pages/Login.jsx
@@ -5,7 +5,7 @@ import AuthIcon from "../components/auth/AuthIcon";
 const Login = () => {
   return (
     <div className="h-screen w-full flex justify-center items-center bg-gradient-to-br from-primary via-secondary to-tertiary ">
-      <div className="flex flex-row w-11/12 lg:w-2/3 max-h-[60vh] bg-gray rounded-lg md:rounded-r-lg">
+      <div className="flex flex-row w-11/12 sm:w-4/5 lg:w-2/3 max-h-[70vh] bg-gray rounded-lg md:rounded-r-lg">
         <div className="w-5/12 hidden md:flex md:justify-center md:items-center overflow-hidden p-1">
           <AuthIcon />
         </div>
