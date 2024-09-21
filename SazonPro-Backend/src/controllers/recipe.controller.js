a8c52073ff8d8ea553e3e28f0a5f49b70f4b8ee5
Fabio-Alfredo <00210121@uca.edu.sv>
2024-09-21 11:00:52 -0600

    update: new metod creation recipe and author marker

--- SazonPro-Backend/src/controllers/recipe.controller.js
+++ SazonPro-Backend/src/controllers/recipe.controller.js
@@ -1,17 +1,22 @@
 import { createRecipe, existsRecipe } from "../services/recipe.service.js";
 import { handdleHttpError, HttpError } from "../utils/error.handdle.js";
 import { existsUser } from "../services/user.service.js";
+import { verifyToken } from "../utils/jwt.handdle.js";
 
 export const createRecipeController = async (req, res) => {
     try {
-        const { title, ingredients, steps, prepTime, cookTime, author } = req.body;
-        const existUser = await existsUser(author);
-        if (!existUser) throw new HttpError(404, "User not found");
+        const { title, ingredients, steps, prepTime, cookTime } = req.body;
+
+        const token = req.headers.authorization.split(' ')[1];
+        
+        const {email, id} = verifyToken(token);
+
+        if (!email) throw new HttpError(404, "User not found");
 
         const existRecipe = await existsRecipe(title);
         if(existRecipe) throw new HttpError(400, "Recipe already exists");
 
-        const response = await createRecipe(title, ingredients, steps, prepTime, cookTime, existUser.id);
+        const response = await createRecipe(title, ingredients, steps, prepTime, cookTime, id);
         res.status(201).send(response);
     } catch (e) {
         handdleHttpError(res, e.message, e)
